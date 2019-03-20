0af1850b6fab6e2f7ef41c356292042f9ec6f387
Dian Mukti Wibowo <dian.wibowo@clickoding.id>
2019-03-20 12:03:40 +0700

    Add Sample Get Current Locations

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, private geo: Geolocation) {
 
+  	setTimeout(() => {
+
+  		this.getLocations();
+
+  	}, 1000);
+
+  }
+
+  getLocations() {
+  	this.geo.getCurrentPosition().then((resp) => {
+	 // resp.coords.latitude
+	 // resp.coords.longitude
+	 
+	 this.alertCtrl.create({
+	 	title: 'My GPS',
+	 	message: 'lat : ' + resp.coords.latitude + ', Lng: ' + resp.coords.longitude,
+	 	enableBackdropDismiss : true
+	 }).present();
+
+	}).catch((error) => {
+	 
+	  console.log('Error getting location', error);
+	
+	});
   }
 
 }
